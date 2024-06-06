bef03f4c12c1376f13e96bc9836305a17dff1012
Prachit Pandit <92202695+prachit082@users.noreply.github.com>
2024-06-06 22:41:42 +0530

    Update wordcloud.py

--- Word-Cloud/wordcloud.py
+++ Word-Cloud/wordcloud.py
@@ -4,6 +4,16 @@ from wordcloud import STOPWORDS
 import matplotlib.pyplot as plt # type: ignore
 import pandas as pd # type: ignore
 
+def generate_word_cloud(csv_file_path):
+    """
+    Generate a word cloud from a CSV file.
+
+    Args:
+        csv_file_path (str): The path to the CSV file.
+
+    Returns:
+        None
+    """
 data = pd.read_csv("you_csv_file_link")
 print(data.head())
 
